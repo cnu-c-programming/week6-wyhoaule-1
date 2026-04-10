7a146fd32c31a90722bc60996801b5ed8e328ab2
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-04-10 10:34:34 +0900

    1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char *names[5] = {"kim", "lee", "park", "choi", "jeong"};
+
+    for (int i=0; i<5; i++) {
+        for (int j=0; ; j++) {
+            char c = *(*(names+i)+j);
+            if (c == '\0')
+                break;
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+}
