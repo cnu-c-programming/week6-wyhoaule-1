1aa6df5f9a7f3275e25195b3f07e230e73b45f7d
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-04-15 10:22:09 +0900

    3-3.ㅊ

---
+++ code/3-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[2][3] = {{1,2,3}, {4,5,6}};
+
+    int (*p1)[2][3] = &arr;
+    int (*p2)[3] = &arr[0];
+    int *p3 = &arr[0][0];
+
+    printf("%p\n", (void*)p1);
+    printf("%p\n", (void*)p2);
+    printf("%p\n", (void*)p3);
+    printf("\n");
+    printf("%llu\n", (unsigned long long)(p1+1) - (unsigned long long)(p1));
+    printf("%llu\n", (unsigned long long)(p2+1) - (unsigned long long)(p2));
+    printf("%llu\n", (unsigned long long)(p3+1) - (unsigned long long)(p3));
+}
